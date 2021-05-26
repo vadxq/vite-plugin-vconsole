3c3c32768d6cfc3c60a1caca51657274340b04dc
vadxq <dongxianlin@vadxq.com>
2021-05-26 12:00:44 +0800

    feat(vconsole): complete the main function

---
+++ example/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { viteVConsole } from '../src';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    viteVConsole({
+      entry: '/Users/vadxq/git/vite-plugin-vconsole/example/src/main.js'
+    })
+  ]
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,46 @@
-const viteVConsole = () => {
-  console.log('viteVConsole');
-};
+(async () => {
+  try {
+    await import('vconsole');
+  } catch (e) {
+    throw new Error(
+      'vite-plugin-vconsole requires vconsole to be present in the dependency tree.'
+    );
+  }
+})();
 
-export default viteVConsole;
+import type { viteVConsoleOptions } from './types';
+import type { Plugin } from 'vite';
+
+import { ResolvedConfig } from 'vite';
+
+export function viteVConsole(opt: viteVConsoleOptions): Plugin {
+  console.log(opt);
+
+  let config: ResolvedConfig;
+
+  return {
+    name: 'vite:vconsole',
+    enforce: 'pre',
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+      console.log(config);
+    },
+    // transformIndexHtml(html) {
+    //   return html.replace(
+    //     /<\/body>/,
+    //     `<script src="https://unpkg.com/vconsole/dist/vconsole.min.js"></script><script>;new window.VConsole();</script></body>`
+    //   )
+    // },
+    transform(_source: string, id: string) {
+      if (id === opt.entry) {
+        console.log(_source);
+        console.log(this);
+        return _source + `import VConsole from 'vconsole';new VConsole();`;
+      }
+      console.log(id);
+      return _source;
+    }
+  };
+}
+
+export * from './types';
