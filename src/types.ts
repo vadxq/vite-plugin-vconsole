13f757a908434319151fc14028310ad9b420c00e
KeJunMao <kejun1997@gmail.com>
2022-10-19 19:11:22 +0800

    chore: use vconsole type

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,9 @@
+import type { VConsoleOptions } from 'core/options.interface';
+import type {} from 'vconsole';
+
 export interface viteVConsoleOptions {
   entry: string[] | string; // entry file
   localEnabled?: boolean; // serve dev enabled
   enabled?: boolean; // enabled
-  config?: voption; // vconsole option
-}
-
-export interface voption {
-  defaultPlugins?: string[];
-  onReady?: () => void;
-  onClearLog?: () => void;
-  maxLogNumber?: number;
-  disableLogScrolling?: boolean;
-  theme?: 'light' | 'dark';
+  config?: VConsoleOptions; // vconsole option
 }
